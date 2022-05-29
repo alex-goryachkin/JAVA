a5515cf829720d8049031cd21727b0d307edc219
Alex <china19881@rambler.ru>
2022-05-29 12:27:53 +0800

    second commit

--- programm.java
+++ programm.java
@@ -1,9 +1,17 @@
+
 /**
  * programm
  */
+import java.util.Scanner;
+
 public class programm {
     public static void main(String[] args) {
-        Sistem.out.print
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("int a: ");
+        int a = iScanner.nextInt();
+        System.out.printf("int b: ");
+        int b = iScanner.nextInt();
+        System.out.printf("%d ^ %d = %f", a, b, Math.pow(a,b));
+        iScanner.close();
     }
-    
 }
